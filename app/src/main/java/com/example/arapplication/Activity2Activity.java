52843c445aef52b0cb37d47ef1a2957b2d4f8bfe
CodeItArYan <52409625+Aryan-Kai@users.noreply.github.com>
2022-08-26 01:00:43 +0530

    Folder added

--- app/src/main/java/com/example/arapplication/Activity2Activity.java
+++ app/src/main/java/com/example/arapplication/Activity2Activity.java
@@ -2,13 +2,26 @@ package com.example.arapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Activity2Activity extends AppCompatActivity {
 
+    ImageView btnpuz,btnquiz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2);
+        btnpuz = findViewById(R.id.imgpuzzel);
+        btnquiz = findViewById(R.id.imgquiz);
+        btnquiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity2Activity.this,AssesmentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
